790d7db3eb9eb7d7e2c07174c066afedcecb4c73
Srajan <srajansharma1005@gmail.com>
2025-04-25 01:24:44 +0530

    new file

---
+++ dev.py
@@ -0,0 +1,7 @@
+
+value = "$7.00"
+helptext = """You can create long strings of text
+spanning several lines by using triple quotes at
+the beginning and end of the text"""
+print(value)
+print(helptext)
